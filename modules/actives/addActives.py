7921ab1f3d2626757912b571a01129c3ef9f61f8
JhoanSLC <JhoanSLC4@gmail.com>
2024-06-19 16:35:58 -0500

    feat: :sparkles: Add actives function :sparkles:

--- modules/actives/addActives.py
+++ modules/actives/addActives.py
@@ -5,20 +5,47 @@ from modules.files import *
 
 def addActives():
     clean()
-    checkFile('actives.json')
-    actives = {
-        "codTransaccion" : "",
-        "nroFormulario" : "",
-        "codCampus" : "",
-        "marca" : "",
-        "categoria" : "",
-        "tipo" : "",
-        "valorUnitario" : 0,
-        "proveedor" : "",
-        "nroSerial" : "",
-        "empresaResponsable" : "",
-        "estado" : "",
-        "historial" : [],
+    checkFile('actives.json') # Check if the actives.json exists and if it doesn't create it 
+    activesData = loadFile('actives.json') # Bring the json data into a variable
+    actives = { # Create the main structure our actives are going to have
+        "transactionCode" : "",
+        "formNumber" : "",
+        "campusCode" : "",
+        "brand" : "",
+        "category" : "",
+        "type" : "",
+        "unitValue" : 0,
+        "provider" : "",
+        "serialNumber" : "",
+        "responsibleCompany" : "",
+        "status" : "", 
+        "history" : [],
     }
-    
+    numberOptions = ["transactionCode",'formNumber','campusCode','serialNumber']
+    for item in actives: # Use a for loop to go through every structure's item 
+        if item != "history": # Checks the "history" so the for loop doesn't create an input for it
+            if item in numberOptions:
+                while True:
+                    try:
+                        clean()
+                        while True:
+                            newValue =  int(input(f'Enter {item}: ')) # Store the new value from an input for each item
+                            if item == "transactionCode":
+                                if newValue in activesData:
+                                    clean()
+                                    print("This transactionCode already exists, please enter a new one")
+                                    pause()
+                                else:
+                                    break
+                        actives[item] = newValue # Update the main structure with the new value 
+                        break
+                    except:
+                        clean()
+                        print(f'Please enter a valid number for {item}')
+                        pause()
+    # Once the for loop ends
+    code = actives["transactionCode"] # Bring the transaction code that's going to be the main id for the json file
+    activesData.update({code : actives}) # Update the activesData we loaded from the json file
+    updateFile('actives.json', activesData) # Update the json file with the new active structure
+        
 
--- modules/files.py
+++ modules/files.py
@@ -10,14 +10,14 @@ def checkFile(file : str):
         with open (BASE+file, 'w') as createFile:
             json.dump({},createFile, indent = 4)
     
-# Load a json file into 
+# Load a json file into a python variable
 # Receive: Json file name
 def loadFile(file : str):
     with open (BASE+file, 'r') as readFile:
         return json.load(readFile)
     
 # Send a new data to the json file
-# Receive: Json
+# Receive: Json file name and the data you will add to that json
 def updateFile(file : str, data):
     with open (BASE+file, 'w+') as updateFile:
-        json.dump(data, updateFile, indent=4)
+        json.dump(data, updateFile, indent = 4)
